3abd4a9ec212d88b61f3075661731e79833f96d5
youu2 <qiyouw1@student.unimelb.edu.au>
2025-07-31 17:19:41 +1000

    QFramwork apply

---
+++ Assets/Scripts/Game/Player.Designer.cs
@@ -0,0 +1,9 @@
+// Generate Id:b96bd56b-e0ed-4258-94a3-9d493c4554a2
+using UnityEngine;
+
+namespace ProjectBlood
+{
+	public partial class Player
+	{
+	}
+}
---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using QFramework;
+
+namespace ProjectBlood
+{
+	public partial class Player : ViewController
+	{
+		void Start()
+		{
+			// Code Here
+			"Hello world".LogInfo();
+		}
+	}
+}
