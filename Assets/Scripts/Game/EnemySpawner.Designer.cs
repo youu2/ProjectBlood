df61bc30a2a12b23f94499a52a232b806aee4f04
youu2 <qiyouw1@student.unimelb.edu.au>
2025-09-10 22:29:36 +1000

    Generating enemies

---
+++ Assets/Scripts/Game/EnemySpawner.Designer.cs
@@ -0,0 +1,11 @@
+// Generate Id:1b1431bf-c6c3-47ab-b230-408fb630fa6c
+using UnityEngine;
+
+namespace ProjectBlood
+{
+	public partial class EnemySpawner
+	{
+		public ProjectBlood.Enemy Enemy;
+		
+	}
+}
---
+++ Assets/Scripts/Game/EnemySpawner.cs
@@ -0,0 +1,73 @@
+// reference from my anothor project abyss(MIGA)
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+	[SerializeField] private GameObject[] normalEnemies;
+	[SerializeField] private Transform player;
+	[SerializeField] private float maxSpawnRadius = 50f;
+	[SerializeField] private float minSpawnRadius = 20f;
+	[SerializeField] private float enemySpawnInterval = 15f;
+	[SerializeField] private float EnemyNum = 3f; // The number of normal enemies generated once
+
+
+	private void Start()
+	{
+		StartCoroutine(SpawnEnemies());
+	}
+
+	private IEnumerator SpawnEnemies()
+	{
+		if (player)
+		{
+			while (true)
+			{
+				// Wait for the specified interval before spawning
+				yield return new WaitForSeconds(enemySpawnInterval);
+
+				for (int i = 0; i <= EnemyNum; i++)
+				{
+					GameObject enemyToSpawn = normalEnemies[Random.Range(0, normalEnemies.Length)];
+
+					// Generate a random position within the specified radius range
+					Vector2 spawnPosition = GetValidSpawnPosition();
+
+					if (enemyToSpawn != null)
+					{
+						Instantiate(enemyToSpawn, spawnPosition, Quaternion.identity);
+					}
+					else
+					{
+						Debug.LogWarning("Enemy to spawn is null.");
+					}
+				}
+				
+			}
+		}
+	}
+	
+
+	// Get a random valid spawn position within the specified min and max radius
+    private Vector2 GetValidSpawnPosition()
+    {
+		if (player)
+		{
+			float distance;
+			// Generate a random angle in radians
+			float angle = Random.Range(0f, Mathf.PI * 2);
+
+			// Generate a random distance between the min and max radius
+			distance = Random.Range(minSpawnRadius, maxSpawnRadius);
+
+
+			// Convert polar coordinates to Cartesian coordinates
+			Vector2 randomPos = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * distance;
+			// Convert the random 2D position into 3D coordinates with the same y as the player
+			return randomPos;
+		}
+		else return new Vector2();
+    }
+
+}
