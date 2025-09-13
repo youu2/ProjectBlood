68c092f0c5d7db599749bd316a18b96c2222ea10
youu2 <qiyouw1@student.unimelb.edu.au>
2025-09-14 03:27:24 +1000

    Remove the redundant code

--- Assets/Scripts/Game/Enemy.cs
+++ Assets/Scripts/Game/Enemy.cs
@@ -41,14 +41,7 @@ namespace ProjectBlood
 				var direction = (Player.player1.transform.position - transform.position).normalized;
 				transform.Translate(direction * Time.deltaTime * moveSpeed);
 			}
-
-			// death of enemy
-			// if (currentHealth <= 0)
-			// {
-			// 	this.DestroyGameObjGracefully();
-			// 	Global.AddExp(1);
-			// 	// UIKit.OpenPanel<UIGamePassPanel>();
-			// }
+			
 		}
 
 		public void TakeDamage(float Damage)
