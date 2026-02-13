3af2906af92923fa56eb87d878e61acec62004bc
youu2 <qiyouw1@student.unimelb.edu.au>
2026-02-13 18:36:42 +1100

    Implement PlayerBullet damage logic; update TagManager for new tags

--- Assets/Scripts/Game/Abilities/PlayerBullet.cs
+++ Assets/Scripts/Game/Abilities/PlayerBullet.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using ProjectBlood;
+using QFramework;
 using UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
 {
     public Vector2 direction;
     public float speed = 10.0f;
+    public float damage = 20.0f;
 
     void Start()
     {
@@ -22,5 +25,25 @@ public class PlayerBullet : MonoBehaviour
         // 默认朝右是 0 度，所以直接用 Atan2 算角度
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    // void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     if (collision.gameObject.CompareTag("Enemy"))
+    //     {
+    //         //UIKit.OpenPanel<UIGamePassPanel>();
+    //         collision.gameObject.GetComponent<Enemy>().TakeDamage(damage);
+    //     }
+    //     //UIKit.OpenPanel<UIGamePassPanel>();
+    // }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            //UIKit.OpenPanel<UIGamePassPanel>();
+            collision.gameObject.GetComponent<Enemy>().TakeDamage(damage);
+        }
+        //UIKit.OpenPanel<UIGamePassPanel>();
     }
 }
