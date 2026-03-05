18852c8e818a1dc49f1ba1fce1cc59c7c0d3e77c
youu2 <w2078349758@gmail.com>
2026-03-06 01:31:47 +1100

    Achieve the effect of the camera smoothly following the player

--- Assets/Scripts/UI/CameraController.cs
+++ Assets/Scripts/UI/CameraController.cs
@@ -17,6 +17,15 @@ public class CameraControler : MonoBehaviour
         {
             return;
         }
-        transform.position = new Vector3(Player.player1.transform.position.x, Player.player1.transform.position.y + 0.5f, transform.position.z);
+        // 当前玩家移动方向
+        Vector2 moveDirection = new Vector2(Player.player1.transform.position.x, Player.player1.transform.position.y);
+        // 获取当前摄像机位置
+        Vector3 currentCameraPosition = transform.position;
+
+        // 摄像机缓动目标位置(调整e的系数越大越慢跟随)
+        Vector3 targetPosition = Vector3.Lerp(currentCameraPosition, new Vector3(moveDirection.x, moveDirection.y, -10), 1.0f - Mathf.Exp(-4.0f * Time.deltaTime));
+        targetPosition.z = -10; // 保持摄像机在正确的深度位置
+        // 摄像机跟随玩家移动
+        transform.position = targetPosition;
     }
 }
